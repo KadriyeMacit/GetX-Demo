13a8048f0ad82cbd3bef449a3326e429f3ce5c53
Kadriye <kadriyemacit9655@gmail.com>
2021-04-04 23:34:16 +0300

    project upload

---
+++ getx_demo/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import './views/my_home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'GetX Demo'),
+    );
+  }
+}
---
+++ getx_demo/lib/views/my_home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/my_home_page_controller.dart';
+import './about_page.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  final MyHomePageController controller = Get.put(MyHomePageController());
+  MyHomePage({this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Obx(
+              () => Text(
+                '${controller.count.value}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+                onPressed: () {
+                  Get.off(AboutPage());
+                },
+                child: Text('About GetX')),
+            TextButton(
+                onPressed: () {
+                  Get.snackbar('GetX Snackbar', 'Yay! Awesome GetX Snackbar',
+                      snackPosition: SnackPosition.BOTTOM,
+                      backgroundColor: Colors.amberAccent);
+                },
+                child: Text('Show Snackbar')),
+            TextButton(
+                onPressed: () {
+                  Get.defaultDialog(
+                      title: 'GetX Alert',
+                      middleText: 'Simple GetX alert',
+                      textConfirm: 'Okay',
+                      confirmTextColor: Colors.amberAccent,
+                      textCancel: 'Cancel');
+                },
+                child: Text('Show AlertDialog'))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
